b5555a5db408b13a80e17d45848043852d882738
Glitch (plausible-gifted-constrictor) <none>
2021-01-23 12:09:15 +0000

    🗃🏛 Checkpoint ./config.js:27740246/118

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 exports.Prefix = `$`; //your prefix for bot
-exports.Token = `Nzk1NjkxNzAwMDk3MTIyNDA0.X_NDpA.A1_j9HUlMjbMTXwhkjLHdI63SRs`; //your token
+exports.Token = `Nzk1NjkxNzAwMDk3MTIyNDA0.X_NDpA.lamNCDWET6pre5xj7nW17kIogBo`; //your token
 exports.Color = `RANDOM`; //color   of embed
